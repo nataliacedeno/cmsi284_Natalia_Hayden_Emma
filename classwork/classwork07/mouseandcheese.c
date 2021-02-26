ac63e51dc6f967288db51f5a28c33d3433e9aae8
emmalee84 <70652239+emmalee84@users.noreply.github.com>
2021-02-26 14:25:32 -0800

    homework07

---
+++ classwork/classwork07/mouseandcheese.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <ctype.h> 
+
+int main() { 
+    const char * word[4]; 
+    word[0] = "HAYDEN";
+    word[1] = "NATALIA";
+    word[2] = "EMMA"; 
+    word[3] = "ALLERGIC"; 
+
+    char a; 
+    
+    srand(time(0));
+    int num = (rand() % 2); 
+
+    char guess = word[num]; 
+
+    char dash = 
+    
+    for (int i=0; i < 10; i++){ 
+    
+    printf("Enter a letter: "); 
+    scanf("%c", &a ); 
+
+    a = toupper(a); 
+
+    if ()
+
+
+    }
+    
+
+
+}
+

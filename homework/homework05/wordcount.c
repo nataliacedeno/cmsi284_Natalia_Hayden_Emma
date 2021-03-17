4882145deae475742983d9bbe40addd4046fbe63
Natalia Cedeno <natalia@Natalias-MacBook-Air.local>
2021-03-17 11:35:11 -0700

    adding wordcount.c

--- homework/homework05/wordcount.c
+++ homework/homework05/wordcount.c
@@ -1,3 +1,42 @@
-int main() {
-    
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    char filename[250];
+    int wordCount = 0;
+
+    if (argc < 2) {
+        printf("   Enter a file name: ");
+        fflush(stdout);
+        scanf("%s", filename);
+        fflush(stdout);
+    // int isspace(int c)
+    } else {
+        strcpy(filename, argv[1]);
+        fflush(stdout);
+    }
+
+    FILE *in = fopen(argv[1], "r");
+    if (!in)
+    {
+        printf("File %s does not exist\n", argv[1]);
+        return 2;
+    }
+
+    while( 1 ) {
+        int c = fgetc( in );
+        if (isspace(c)) {
+            wordCount++;
+        }
+        if( c == EOF ) break;
+    }
+
+    wordCount++;
+
+    printf("Word count: %d", wordCount);
+
+    fclose(in);
+    exit(0);
 }

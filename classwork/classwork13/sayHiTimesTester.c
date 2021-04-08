713b55210065d74abbf16bb935f5782fbd8e5a72
emmalee84 <70652239+emmalee84@users.noreply.github.com>
2021-04-08 15:18:08 -0700

    classwork 13 commit

---
+++ classwork/classwork13/sayHiTimesTester.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+#include <inttypes.h>
+
+int64_t sayHiTimes(int64_t x);
+
+int main () {
+
+    printf(sayHiTimes(1));
+    printf(sayHiTimes(2));
+    printf(sayHiTimes(3));
+    printf(sayHiTimes(4));
+    printf(sayHiTimes(5));
+
+
+}

bbef39ecb0b1cdcb809394841e8835c0eb63ec1a
emmalee84 <70652239+emmalee84@users.noreply.github.com>
2021-04-22 14:21:39 -0700

    new nbo

---
+++ homework/homework06/makeNBOC.h
@@ -0,0 +1,13 @@
+#ifndef MAKENBOC_DOT_H    /* This is an "include guard" */
+#define MAKENBOC_DOT_H    /* prevents the file from being included twice. */
+                     /* Including a header file twice causes all kinds */
+                     /* of interesting problems.*/
+#include <inttypes.h>
+
+/**
+ * This is a function declaration.
+ * It tells the compiler that the function exists somewhere.
+ */
+uint32_t makeNBOC(uint32_t num);
+
+#endif /* MAKENBO_DOT_H */
